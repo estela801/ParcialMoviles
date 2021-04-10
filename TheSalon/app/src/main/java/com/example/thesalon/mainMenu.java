2d3e1e85bd51b4f5e93747f58d98d874ef6cd33d
estela801 <ros98cg98@gmail.com>
2021-04-10 01:18:05 -0600

    login

--- TheSalon/app/src/main/java/com/example/thesalon/mainMenu.java
+++ TheSalon/app/src/main/java/com/example/thesalon/mainMenu.java
@@ -6,14 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class mainMenu extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private TextView userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_menu);
+        userName=findViewById(R.id.txtvUserName);
+
+        //FIrebase
+        mAuth=FirebaseAuth.getInstance();
+        FirebaseUser currentUser= mAuth.getCurrentUser();
+        userName.setText(currentUser.getDisplayName());
     }
 
     //go to addDate1
